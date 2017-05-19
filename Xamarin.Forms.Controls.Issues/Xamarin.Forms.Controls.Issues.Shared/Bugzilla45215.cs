69731be3d0c33fdbf6758bc6d3f8f330d264a7e5
E.Z. Hart <hartez@gmail.com>
2017-05-18 19:32:32 -0600

    Force page layout update when navigating back from a keyboard-resized page

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Bugzilla45215.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Bugzilla45215.cs
@@ -80,7 +80,7 @@ namespace Xamarin.Forms.Controls.Issues.Xamarin.Forms.Controls.Issues
 					"Tap on the control to bring up the software keyboard, then use the hardware 'Back' button to dismiss the keyboard. If the part of the page where the keyboard was is not green, the test has failed."
 			};
 
-			var layout = new Grid
+			var layout = new StackLayout
 			{
 				Children = { instructions, view() },
 				HorizontalOptions = LayoutOptions.Fill,
