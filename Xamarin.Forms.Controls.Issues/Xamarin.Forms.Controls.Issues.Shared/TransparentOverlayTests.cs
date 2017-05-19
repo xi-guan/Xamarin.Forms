1aafac9624cc83fd8b7d17bf760ce3186d52862b
E.Z. Hart <hartez@gmail.com>
2017-05-19 16:22:53 -0600

    Opacity?

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/TransparentOverlayTests.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/TransparentOverlayTests.cs
@@ -8,7 +8,6 @@ namespace Xamarin.Forms.Controls.Issues
 	[Issue(IssueTracker.None, 618, "Transparent Overlays", PlatformAffected.All)]
 	public class TransparentOverlayTests : TestContentPage
 	{
-		// Need a grid with two rows, controls in each, and an overlaying stacklayout
 		protected override void Init()
 		{
 			var grid = new Grid
@@ -25,10 +24,13 @@ namespace Xamarin.Forms.Controls.Issues
 				HorizontalOptions = LayoutOptions.Center,
 				VerticalOptions = LayoutOptions.Center
 			};
-			button1.Clicked += (sender, args) => { Debug.WriteLine($">>>>> TransparentOverlayTests Init 81: Button1"); };
+			button1.Clicked += (sender, args) =>
+			{
+				// This line executes on Android, but not on iOS or UWP
+				Debug.WriteLine($">>>>> TransparentOverlayTests Init 81: Button1");
+			};
 			grid.Children.Add(button1);
 			Grid.SetRow(button1, 0);
-
 
 			var button2 = new Button
 			{
@@ -36,26 +38,33 @@ namespace Xamarin.Forms.Controls.Issues
 				HorizontalOptions = LayoutOptions.Center,
 				VerticalOptions = LayoutOptions.Center
 			};
-			button2.Clicked += (sender, args) => { Debug.WriteLine($">>>>> TransparentOverlayTests Init 81: Button2"); };
+			button2.Clicked += (sender, args) =>
+			{
+				// This line doesn't execute on any platform
+				Debug.WriteLine($">>>>> TransparentOverlayTests Init 38: Button2");
+			};
 			grid.Children.Add(button2);
 			Grid.SetRow(button2, 1);
 
+			// Put a stack layout over the button in the top half of the grid
 			var layout1 = new StackLayout
 			{
 				HorizontalOptions = LayoutOptions.Fill,
 				VerticalOptions = LayoutOptions.Fill,
-				BackgroundColor = Color.Transparent
+				BackgroundColor = Color.Transparent, // and make it "transparent",
+				Opacity = 0
 			};
 
 			grid.Children.Add(layout1);
 			Grid.SetRow(layout1, 0);
 
+			// Put a stack layout over the button in the bottom half of the grid
 			var layout2 = new StackLayout
 			{
 				HorizontalOptions = LayoutOptions.Fill,
 				VerticalOptions = LayoutOptions.Fill,
-				BackgroundColor = Color.Blue,
-				Opacity = 0.2
+				BackgroundColor = Color.Blue, // make it "not transparent"
+				Opacity = 0 
 			};
 
 			grid.Children.Add(layout2);
